de3b60e07b11e4ab94184ece2f6add61836acc73
sfauzii <sjahrulfauzii@gmail.com>
2025-09-26 13:01:49 +0700

    refactor: routing page

---
+++ cg-next-gsap-nav/src/app/contact/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <div className="page-content">
+            <h1>
+                Contact<sup>(05)</sup>
+            </h1>
+            
+        </div>
+    )
+}
+
+export default page;
---
+++ cg-next-gsap-nav/src/app/lab/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <div className="page-content">
+            <h1>
+                Lab<sup>(04)</sup>
+            </h1>
+            
+        </div>
+    )
+}
+
+export default page;
