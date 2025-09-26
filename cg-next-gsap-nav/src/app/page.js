50a8a1ca01d262adf9b9177f2f55bffbaa401b60
sfauzii <sjahrulfauzii@gmail.com>
2025-09-26 12:50:43 +0700

    refactor: routing

--- cg-next-gsap-nav/src/app/page.js
+++ cg-next-gsap-nav/src/app/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="page-content hero">
+      <h1>
+        Index<sup>(01)</sup>
+      </h1>
+
+    </div>
   );
 }
